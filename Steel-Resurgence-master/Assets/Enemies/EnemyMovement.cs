ceed242f7102a70baf82bfd24935eccd9f5a2f50
Juan Pablo Soto Rojas <juanpablosoto100@gmail.com>
2023-09-09 23:41:31 -0600

    a

---
+++ Steel-Resurgence-master/Assets/Enemies/EnemyMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform target; //punto a donde se dirige el enemigo
+    private int pathIndex = 0; //indice del camino que sigue el enemigo
+
+    private void Start()
+    {
+        target = LevelManager.main.path[pathIndex];
+    }
+    private void Update()
+    {
+        if (Vector2.Distance(target.position, tranform.position) <= 0.1f)
+        {
+            pathIndex++;
+            target = LevelManager.main.path[pathIndex];
+
+            if (pathIndex == LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                target = LevelManager.main.path[pathIndex];
+            }   
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.velocity = direction * moveSpeed;
+    }
+}
+
+     
---
+++ Steel-Resurgence-master/Assets/Maps/Path.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    [SerializeField]
+    List<Vector3> positions;
+
+    [SerializeField]
+    Vector3 currPosition;
+
+    [SerializeField]
+    Color NodeColor;
+
+    private bool _gameStarted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameStarted = true;
+        currPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!_gameStarted && transform.hasChanged) {
+            currPosition = transform.position;
+        }
+
+        for (int i = 0; i < positions.Count; i++) {
+            Gizmos.color = Color.black;
+            Gizmos.DrawWireSphere(positions[i] + currPosition, 0.5f);
+            if (i < positions.Count - 1) {
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawLine(positions[i] + currPosition, positions[i + 1] + currPosition);
+            }
+        }
+    }
+}
